a64a8814a69bca1c868ad88ebfc1797dc525ae03
Shaun Savage <swansavage@gmail.com>
2020-08-27 16:53:40 -0500

    added api fetch and event listeners to display correct data and implement functionality

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the dom
 const calculate = () => {
-    console.log('test success');
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const rate = data.rates[currency_two];
+
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        });
 };
 
 // Event listeners
@@ -17,5 +28,12 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+});
 
 calculate();
